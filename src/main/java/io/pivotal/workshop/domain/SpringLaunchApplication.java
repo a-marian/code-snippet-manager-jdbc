c5600540d3fe8cfa0071a6f4558b255163eb113f
marelas <marela.as@gmail.com>
2018-08-23 08:29:23 -0300

    snippet spring jdbc

---
+++ src/main/java/io/pivotal/workshop/domain/SpringLaunchApplication.java
@@ -0,0 +1,16 @@
+package io.pivotal.workshop.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class SpringLaunchApplication 
+{
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaunchApplication.class, args);
+	}
+}
